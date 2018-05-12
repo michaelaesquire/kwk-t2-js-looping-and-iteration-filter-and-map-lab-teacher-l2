fad5a83079ae3411c45a6ebae85f2eb41789c2aa
Michaela Olson <mco44@cornell.edu>
2018-05-12 19:24:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, amt) {
+  let matchyBois = drivers.filter(function(matches) {
+    return matches.revenue > amt;
+  });
+  return matchyBois;
+}
+
+function driverNamesWithRevenueOver(drivers, amt) {
+  let matchyBois = drivers.filter(function(matches) {
+    return matches.revenue > amt;
+  });
+//  return matchyBois;
+  let namesOnly = matchyBois.map(function (drive) {
+  named = drive.name;
+  return named;
+  });
+  return namesOnly;
+}
+
+function exactMatch(drivers, key) {
+  let theKey = Object.keys(key);
+//  console.log(theKey);
+
+  let matchyBois = drivers.filter(function(matches) {
+    return matches[theKey] === key[theKey];
+  });
+  return matchyBois;
+}
+
+function exactMatchToList(drivers,key) {
+  let theKey = Object.keys(key);
+//  console.log(theKey);
+
+  let matchyBois = drivers.filter(function(matches) {
+    if ( matches[theKey] === key[theKey]) {
+      return matches[theKey];
+    }
+  });
+//  return matchyBois;
+  let namesOnly = matchyBois.map(function (drive) {
+    named = drive.name;
+    return named;
+  });
+  return namesOnly;
+}
