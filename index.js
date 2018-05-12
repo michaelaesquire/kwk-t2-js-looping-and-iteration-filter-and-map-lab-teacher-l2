50e8e2c0ce6b6664eae3b48261094d2544a84f58
Michaela Olson <mco44@cornell.edu>
2018-05-12 19:08:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, amt) {
   let matchyBois = drivers.filter(function(matches) {
-    return matches > amt;
+    return matches.revenue > amt;
   });
   return matchyBois;
 }
