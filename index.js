e6889c0bdf5bcc2420fd322dca36554c58a1e746
Michaela Olson <mco44@cornell.edu>
2018-05-12 19:13:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function driverNamesWithRevenueOver(drivers, amt) {
 
 function exactMatch(drivers, key) {
   let matchyBois = drivers.filter(function(matches) {
-    return matches.revenue > amt;
+    return matches.name === key;
   });
   return matchyBois;
 }
